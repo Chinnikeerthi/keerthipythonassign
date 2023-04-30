89f2442f1788937245526be450894c57b4ed854d
Chinnikeerthi <keerthireddyg098@gmail.com>
2023-04-30 09:56:51 +0530

    python2

--- python1.py
+++ python1.py
@@ -1,9 +1,9 @@
-book1price = 499.00   #cost of python programming book
-book2price = 855.00   #cost of python libraries cookbook
-book3price = 645.00  #cost of data science in python book
+book1price = 499   #cost of python programming book
+book2price = 855   #cost of python libraries cookbook
+book3price = 645 #cost of data science in python book
 totalprice = book1price+book2price+book3price #total price of 3 books
 gst = 0.12 #gst included
-delivery = 250.00 #delivery charges
+delivery = 250 #delivery charges
 taxamount = gst * totalprice  #total tax for 3 books
 totalamount = taxamount + delivery + totalprice #total amount for 3 books
 print(totalamount)
---
+++ python2.py
@@ -0,0 +1,6 @@
+a=input("enter a string :")
+uniqueletters=set(a.upper()) #convet leeters into upper case
+print("uniqueleeters:",",".join(sorted(uniqueletters)))
+
+
+
